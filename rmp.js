4ced33019e656278b3ede142f4618ccf15be8d5b
heidegg <situles@outlook.fr>
2017-01-28 18:47:38 +0100

    Add files via upload

--- rmp.js
+++ rmp.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var app     = express();
 var Twitter = require('twitter'); // appelle API TWITTER
 
 var twitter = new Twitter({  // clées personelles
@@ -5,6 +7,13 @@ var twitter = new Twitter({  // clées personelles
     consumer_secret: 'eCglpzfp3GWsfylq8TBhovAqgOE0qcBT5W8ATvigxCsH2EfMBB',
     access_token_key: '823903489847922688-EUTrtsDkv6BD2Wg7xALaSS0QMY7NqVh',
     access_token_secret: '9uxSqYQ0NoVcYEgzIREncwFiM23PEKIb43Vx6WHL7Ex23'
+});
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 
@@ -28,7 +37,7 @@ twitter.stream('statuses/filter', { track: '#retirezmoiphotoshop' }, // stream O
       }
     }
   })
-}).listen(process.env.PORT || 5000);
+});
         
     
     
